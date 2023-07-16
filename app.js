921df3235f376ffe9caac3f241af0d19b9beef4f
Dhruv <dhruvsuvarna2019@gmail.com>
2023-07-16 15:36:45 +0530

    latest

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
-const router = express.Router()
 const port = 5000
 
 // Static Files
@@ -18,14 +17,10 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // Routes
-// const newsRouter = require('./src/routes/news')
+const newsRouter = require('./src/routes/news')
 
-// app.use('/', newsRouter)
+app.use('/', newsRouter)
 // app.use('/article', newsRouter)
-
-router.get("/", (req, res)=>{
-    res.render("index", {title: "My Website", abstract: "My wife is a demon queen"})
-})
 
 // Listen on port 5000
 app.listen(process.env.PORT || port, () => console.log(`Listening on port ${port}`))
